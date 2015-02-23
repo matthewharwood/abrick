ff1db8445249bb88460cbbc42da03decde326811
ryan-ko <ryanko.780513@gmail.com>
2015-02-23 12:31:17 -0800

    Added basic setup for camera capturing feature

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -28,6 +28,14 @@ angular.module('abrickApp')
     var audioSelect = document.querySelector("select#audioSource");
     var videoSelect = document.querySelector("select#videoSource");
     var startButton = document.querySelector("button#start");
+
+    /**
+     * Canvas for showing captured image
+     */
+    var canvas = document.querySelector('canvas');
+    var ctx = canvas.getContext('2d');
+    var localMediaStream = null;
+
     var videoSourceValues = [];
     navigator.getUserMedia = navigator.getUserMedia ||
     navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
@@ -96,16 +104,38 @@ angular.module('abrickApp')
         alert('This browser does not support MediaStreamTrack.\n\nTry Chrome Canary.');
       } else {
         MediaStreamTrack.getSources(gotSources);
-        start();
       }
     }
 
     checkDeviceHardwareAndStartCapturing();
-  
+    start();
+
+    var video = document.querySelector('video');
+    var canvas = document.querySelector('canvas');
+    var ctx = canvas.getContext('2d');
+    var localMediaStream = null;
+
+    function snapshot() {
+      if (localMediaStream) {
+        ctx.drawImage(video, 0, 0);
+        // "image/webp" works in Chrome.
+        // Other browsers will fall back to image/png.
+        document.querySelector('img').src = canvas.toDataURL('image/webp');
+      }
+    }
+
+    document.getElementById("capture-photo-btn").onclick = function fun() {
+      snapshot();
+    }
+
+    // Not showing vendor prefixes or code that works cross-browser.
+    navigator.getUserMedia({video: true}, function(stream) {
+      video.src = window.URL.createObjectURL(stream);
+      localMediaStream = stream;
+    }, errorCallback);
 
 
     var syncObject = sync.$asObject();
-
 
     //binds the scope.data to the database as so: 
     //$firebase(new Firebase(url).$asObject().$bindTo($scope, 'data'))
