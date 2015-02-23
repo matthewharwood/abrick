c2e387a6cdfdb8e2a54634d569c864e007aa4cca
Matthew Harwood <matthhar12@gmail.com>
2015-02-23 11:45:15 -0800

    added firebase prop

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -20,6 +20,8 @@ module.exports = function(config) {
     files: [
       // bower:js
       'bower_components/angular/angular.js',
+      'bower_components/firebase/firebase.js',
+      'bower_components/angularfire/dist/angularfire.js',
       'bower_components/angular-animate/angular-animate.js',
       'bower_components/angular-aria/angular-aria.js',
       'bower_components/angular-cookies/angular-cookies.js',
